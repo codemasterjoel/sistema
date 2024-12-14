f5bc92da27cc7ab2de3e137609f9783e3218ea6e
codemasterjoel <codemaster19@gmail.com>
2024-12-13 22:36:02 -0400

    carnet 2

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -235,7 +236,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
