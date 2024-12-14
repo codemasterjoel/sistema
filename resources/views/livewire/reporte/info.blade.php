37debf32acdc6566c8435c80c55978493dcd8ece
codemasterjoel <codemaster19@gmail.com>
2024-12-13 23:16:27 -0400

    corregido infoqr

--- resources/views/livewire/reporte/info.blade.php
+++ resources/views/livewire/reporte/info.blade.php
@@ -3,108 +3,31 @@
       <div class="section">
         <h2 class="title text-center"></h2>
         <div class="team">
-            <div class="rows" style="align-content: center">
+            <div class="row" style="align-content: center">
               <img src="{{asset('img/logocenso.png')}}" width="200" style="display: block; margin:auto">
             </div>
-            <div class="rows">
-              <div class="col-sm-3">
-                <div class="form-group label-floating">
-                  <label class="bmd-label-floating">CÉDULA:</label>
-                  <label class="text-dark">{{ $lsb->cedula }}</label>
-                </div>
+            <div class="row mx-28" style="align-content: center">
+              <img src="{{asset('assets/img/marie.jpg')}}" class=" rounded-full mx-28" style="display: block; margin:auto">
+            </div>
+            <div class="row mt-4">
+              <div class="col-3">
+                  <h1 class="font-bold text-neutral-950">CÉDULA:</h1>
               </div>
-              <div class="col-sm-4">
-                <table>
-                  <tr>
-                    <td>                      
-                      <ol class="switches">
-                        <li>
-                          <input type="checkbox" id="estatus" name="estatus" value="1" {{ $lsb->estatus ? 'checked' : '' }} @disabled(true)>
-                          <label for="estatus">
-                            <span>¿EL LSB SE ENCUENTRA ACTIVO?</span>
-                            <span></span>
-                          </label>
-                        </li>
-                      </ol>
-                    </td>
-                  </tr>
-                </table>
+              <div class="col-6">
+                <h1 class="font-bold text-neutral-950">{{ $lsb->cedula }}</h1>
               </div>
             </div>
-            <div class="rows">
-                <div class="col-sm-5">
-                  <div class="form-group">
-                    <label>NOMBRES:</label>
-                    <label style="margin-top: 25px" id="labelNombreCompleto" name="labelNombreCompleto" class="text-dark">{{ $lsb->NombreCompleto}}</label>
-                  </div>
+            <div class="row mt-4">
+                <div class="col-3 font-bold">
+                    <h1 class="text-neutral-950">NOMBRES:</h1>
                 </div>
-                <div class="col-sm-2">
-                  <div class="form-group">
-                    <label>FECHA DE NAC.:</label>
-                    <label style="margin-top: 25px" id="labelFechaNac" name="labelFechaNac" class="text-dark">{{ $lsb->fecha_nac }}</label>
-                  </div>
-                </div>
-                <div class="col-sm-3">
-                    <div class="form-group">
-                        <label>GENERO:</label>
-                        <label style="margin-top: 25px" id="labelGenero" name="labelGenero" class="text-dark">{{ $lsb->genero->nombre }} </label>
-                    </div>
-                </div>
-                <div class="col-sm-2">
-                  <div class="form-group">
-                    <label>TELEFONO:</label>
-                    <label class="text-dark" id="telefono" name="telefono" onkeypress="$(this).mask('(0000) 000-0000')">{{ $lsb->telefono }}</label>
-                  </div>
+                <div class="col-9 font-bold">
+                  <h1 class="text-neutral-950">{{ $lsb->nombre}} {{ $lsb->apellido}}</h1>
                 </div>
             </div>
-            <div class="rows">
-                <div class="col-sm-4">
-                  <div class="form-group">
-                    <label>ESTADO:</label>
-                    <label class="text-dark">{{ $lsb->estado->nombre }}</label>
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="form-group">
-                    <label>MUNICIPIO:</label>
-                    <label class="text-dark">{{ $lsb->municipio->nombre }}</label>
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="form-group">
-                      <label>PARROQUIA:</label>
-                      <label class="text-dark">{{ $lsb->parroquia->nombre }}</label>
-                  </div>
-                </div>
+            <div class="row mt-4 mb-4">
+              <h1 class=" text-neutral-950">Este Persona se encuentra registrada en la Organización Frente Francisco de Miranda como miembro activo.</h1>
             </div>
-            <div class="rows">
-                <div class="col-sm-5">
-                    <div class="form-group">
-                        <label for="nivelacademico">NIVEL ACADEMICO:</label>
-                        <label class="text-dark">{{ $lsb->nivelAcademico->nombre }}</label>
-                    </div>
-                </div>
-                <div class="col-sm-5">
-                    <div class="form-group">
-                        <label for="responsabilidad">RESPONSABILIDAD:</label>
-                        <label class="text-dark">{{ $lsb->responsabilidad->nombre }}</label>
-                    </div>
-                </div>
-                <div class="col-sm-2">
-                    <div class="form-group">
-                        <label for="avanzada">AVANZADA</label>
-                        <label class="text-dark">{{ $lsb->avanzada->nombre }}</label>
-                    </div>
-                </div>
-            </div>
-            <div class="rows">
-                <div class="col-sm-6">
-                    <div class="form-group">
-                        <label for="correo">CORREO:</label>
-                        <label class="text-dark">{{ $lsb->correo }}</label>
-                    </div>
-                </div>
-            </div><br>
         </div>
       </div>
     </div>
